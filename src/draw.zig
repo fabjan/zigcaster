8d3ff278b560e52ea4ee9d8627290a5b4f4dfd55
Fabian <fabian@fmbb.se>
2022-06-29 20:36:29 +0200

    part 1, step 1: Save an image to disk

---
+++ src/draw.zig
@@ -0,0 +1,60 @@
+const std = @import("std");
+const mem = std.mem;
+
+const assert = std.debug.assert;
+
+/// Pack the given color components in one integer value.
+/// 
+pub fn pack_color(r: u8, g: u8, b: u8, a: u8) u32 {
+    const buf = [4]u8{r, g, b, a};
+    const color = mem.bytesToValue(u32, &buf);
+    return mem.bigToNative(u32, color);
+}
+
+pub fn unpack_color(color: u32) [4]u8 {
+    const tmp = mem.nativeToBig(u32, color);
+    return mem.toBytes(tmp);
+}
+
+pub fn drop_ppm_image(writer: anytype, image: []u32, w: usize, h: usize) anyerror!void {
+    assert(image.len == w*h);
+
+    try writer.print("P6\n{} {}\n255\n", .{ w, h });
+
+    for (image) |color| {
+        const pixel = unpack_color(color);
+        try writer.writeAll(pixel[0..3]);
+    }
+}
+
+test "can unpack black" {
+    const zero: u8 = 0;
+    const pixel = unpack_color(0x00000000);
+    try std.testing.expectEqual(zero, pixel[0]);
+    try std.testing.expectEqual(zero, pixel[1]);
+    try std.testing.expectEqual(zero, pixel[2]);
+    try std.testing.expectEqual(zero, pixel[3]);
+}
+
+test "can pack white" {
+    const color = pack_color(255, 255, 255, 255);
+    try std.testing.expectEqual(@as(u32, 0xFFFFFFFF), color);
+}
+
+test "can pack red" {
+    const color = pack_color(255, 0, 0, 255);
+    try std.testing.expectEqual(@as(u32, 0xFF0000FF), color);
+}
+
+test "can pack salmon" {
+    const color = pack_color(0xFA, 0x80, 0x72, 0xFF);
+    try std.testing.expectEqual(@as(u32, 0xFA8072FF), color);
+}
+
+test "can unpack salmon" {
+    const pixel = unpack_color(0xFA8072FF);
+    try std.testing.expectEqual(@as(u8, 0xFA), pixel[0]);
+    try std.testing.expectEqual(@as(u8, 0x80), pixel[1]);
+    try std.testing.expectEqual(@as(u8, 0x72), pixel[2]);
+    try std.testing.expectEqual(@as(u8, 0xFF), pixel[3]);
+}
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,36 @@
 const std = @import("std");
+const fs = std.fs;
+
+const draw = @import("./draw.zig");
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
-}
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
 
-test "basic test" {
-    try std.testing.expectEqual(4711, 1337 + 42*80 + 42/3);
+    const allocator = arena.allocator();
+
+    const win_w: usize = 512;
+    const win_h: usize = 512;
+
+    var framebuffer = try allocator.create([win_w*win_h]u32);
+
+    for (framebuffer) |_, x| {
+        const i = x % win_w;
+        const j = x / win_h;
+        const r = 255.0 * @intToFloat(f32, j)/@intToFloat(f32, win_h);
+        const g = 255.0 * @intToFloat(f32, i)/@intToFloat(f32, win_w);
+        const b = 0.0;
+
+        framebuffer[x] = draw.pack_color(
+            @floatToInt(u8, r),
+            @floatToInt(u8, g),
+            @floatToInt(u8, b),
+            255,
+        );
+    }
+
+    const imageFile = try fs.cwd().createFile("out.ppm", .{});
+    defer imageFile.close();
+
+    try draw.drop_ppm_image(imageFile.writer(), framebuffer[0..], win_w, win_h);
 }
