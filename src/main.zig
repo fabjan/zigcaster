eb059ef626f8c35125cea42951efcd3d2e6b3e92
Fabian <fabian@fmbb.se>
2022-07-01 23:35:32 +0200

    part 1, step 6: 3D!

--- src/main.zig
+++ src/main.zig
@@ -23,8 +23,11 @@ pub fn main() anyerror!void {
 
     const allocator = arena.allocator();
 
-    const win_w: usize = 512;
+    const win_w: usize = 1024;
     const win_h: usize = 512;
+    const grey = graphics.pack_color(160, 160, 160, 255);
+    const white = graphics.pack_color(255, 255, 255, 255);
+    const cyan = graphics.pack_color(0, 255, 255, 255);
 
     var framebuffer = try allocator.create([win_w * win_h]u32);
 
@@ -33,22 +36,15 @@ pub fn main() anyerror!void {
     const player_y: f32 = 2.345;
     const player_a: f32 = 1.523;
     const fov: f32 = math.pi / 3.0;
+    const max_dist: f32 = math.sqrt(2.0 * float(map.width * map.width));
 
-    // draw backdrop gradient
-    for (framebuffer) |_, x| {
-        const i = x % win_w;
-        const j = x / win_h;
-        const r = 255.0 * float(j) / float(win_h);
-        const g = 255.0 * float(i) / float(win_w);
-        const b = 0.0;
-
-        framebuffer[x] = graphics.pack_color(byte(r), byte(g), byte(b), 255);
+    // fill background
+    for (framebuffer) |_, i| {
+        framebuffer[i] = white;
     }
 
-    const rect_w = win_w / map.width;
+    const rect_w = win_w / (map.width * 2);
     const rect_h = win_h / map.height;
-    const cyan = graphics.pack_color(0, 255, 255, 255);
-    const white = graphics.pack_color(255, 255, 255, 255);
 
     // draw map walls
     {
@@ -71,24 +67,36 @@ pub fn main() anyerror!void {
 
     // camera calculations
     var i: usize = 0;
-    while (i < win_w) : (i += 1) {
-        const ray_offset = fov * float(i) / float(win_w);
+    while (i < win_w / 2) : (i += 1) {
+        const ray_offset = fov * float(i) / float(win_w / 2);
         const angle = player_a - fov / 2 + ray_offset;
 
-        var c: f32 = 0.0;
-        while (c < 20.0) : (c += 0.05) {
-            const cx = player_x + c * math.cos(angle);
-            const cy = player_y + c * math.sin(angle);
-            if (map.data[int(cx) + int(cy) * map.width] != ' ') break;
+        var t: f32 = 0.0;
+        while (t < max_dist) : (t += 0.05) {
+            const cx = player_x + t * math.cos(angle);
+            const cy = player_y + t * math.sin(angle);
 
+            // visibility cone
             const pix_x = int(cx * float(rect_w));
             const pix_y = int(cy * float(rect_h));
-            framebuffer[pix_x + pix_y * win_w] = white;
+            framebuffer[pix_x + pix_y * win_w] = grey;
+
+            // "3D" view
+            if (map.data[int(cx) + int(cy) * map.width] != ' ') {
+                const column_height = int(float(win_h) / t);
+                graphics.draw_rectangle(
+                    framebuffer, win_w, win_h,
+                    win_w / 2 + i, win_h / 2 - column_height / 2,
+                    1, column_height,
+                    cyan
+                );
+                break;
+            }
         }
     }
 
     // render output
-    const imageFile = try fs.cwd().createFile("out_5.ppm", .{});
+    const imageFile = try fs.cwd().createFile("out_6.ppm", .{});
     defer imageFile.close();
     try graphics.drop_ppm_image(imageFile.writer(), framebuffer[0..], win_w, win_h);
 }
